452f75cde4fd7dcb5c4658735479f3daf74fb898
Philippe Hébert <philippe.heb@gmail.com>
2021-10-13 13:23:41 +0200

    first commit

---
+++ keystone.ts
@@ -0,0 +1,56 @@
+import "dotenv/config";
+import { createAuth } from "@keystone-next/auth";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long should user stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    //TODO: Add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      //TODO: Add data seeding here
+    },
+    lists: createSchema({
+      User,
+      Product,
+    }),
+    ui: {
+      //TODO: change this for roles
+      isAccessAllowed: ({ session }) => {
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      //GraphQl query
+      User: `id name email`,
+    }),
+  })
+);
---
+++ schemas/Product.ts
@@ -0,0 +1,23 @@
+import { list } from '@keystone-next/keystone/schema';
+import { text, select } from '@keystone-next/fields';
+
+export const Product = list({
+  //access:
+  //ui
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+    }),
+  },
+});
