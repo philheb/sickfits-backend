b20029f3d4d06b04f8e8b1a505f780d887304869
Philippe Hébert <philippe.heb@gmail.com>
2021-10-19 11:28:30 +0200

    added data seeding

--- keystone.ts
+++ keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -37,7 +38,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      //TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the DB');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       User,
